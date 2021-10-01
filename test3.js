7f57e64b19056ee99ae1a8451e72182829b5f3b2
bryantkrause <bryantkrause@gmail.com>
2021-10-01 16:38:13 -0700

    added SO of get last row

--- test3.js
+++ test3.js
@@ -10,3 +10,5 @@ sheet['A2'] = sheet['A1']
 // if the file name below matches file name above this will currently write on the requested cell
 // if below is different file name will rewrite with updated data on the requested cell in the new file name
 XLSX.writeFile(table, 'test1.xlsx');
+
+//  try this place https://stackoverflow.com/questions/57479988/how-can-i-get-the-count-of-rows-in-my-uploaded-excel
