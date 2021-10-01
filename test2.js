fe53cc7430e6b745c7418de7ef1c5fa55a5c0d5f
bryantkrause <bryantkrause@gmail.com>
2021-10-01 12:37:57 -0700

    test3 is the working model to read and write

--- test2.js
+++ test2.js
@@ -4,7 +4,7 @@
 const XLSX = require('xlsx');
 
 /* read workbook */
-const workbook = XLSX.readFile('test1.xls');
+const workbook = XLSX.readFile('test1.xlsx');
 
 /* get the first worksheet */
 const sheet_name_list = workbook.SheetNames;
@@ -22,9 +22,9 @@ console.log(Sheet1A1.v);
 
 // You need to supply your own object with minimum a type and a value. So:
 
-sheet.A1 = { t: 'n', v: 123 };  // Create A1 as a number
-sheet.B1 = { t: 's', v: 'foo' };  // Create B1 as a string
+worksheet.A1 = { t: 'n', v: 123 };  // Create A1 as a number
+worksheet.B1 = { t: 's', v: 'foo' };  // Create B1 as a string
 // Edit: this method does not seem to create new rows if the rows do not already exist. To write single cells and create new rows as required I am now using:
 
-XLSX.utils.sheet_add_aoa(sheet, [[123]], {origin: 'A1'});
-XLSX.utils.sheet_add_aoa(sheet, [['foo']], {origin: 'B1'});
+XLSX.utils.sheet_add_aoa(worksheet, [[123]], {origin: 'A1'});
+XLSX.utils.sheet_add_aoa(worksheet, [['foo']], {origin: 'B1'});
---
+++ test3.js
@@ -0,0 +1,12 @@
+const XLSX = require('xlsx');
+const table = XLSX.readFile('test1.xlsx');
+// Use first sheet
+const sheet = table.Sheets[table.SheetNames[0]];
+// Option 1: If you have numeric row and column indexes
+sheet[XLSX.utils.encode_cell({r: 1 /* 2 */, c: 2 /* C */})] = {t: 's' /* type: string */, v: 'abc123' /* value */};
+// Option 2: If you have a cell coordinate like 'C2' or 'D15'
+// sheet['A1'] = {t: 's' /* type: string */, v: 'abc123' /* value */};
+sheet['A2'] = sheet['A1']
+// if the file name below matches file name above this will currently write on the requested cell
+// if below is different file name will rewrite with updated data on the requested cell in the new file name
+XLSX.writeFile(table, 'test1.xlsx');
